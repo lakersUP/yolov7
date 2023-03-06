f058ce8478dfece6651a4f1c17d1a6c3715c8157
chenyw <chenyw9799@163.com>
2023-03-06 16:37:15 +0800

    分别增加四中不同的注意力机制

--- train.py
+++ train.py
@@ -527,10 +527,10 @@ def train(hyp, opt, device, tb_writer=None):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='yolo7.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='cfg/training/yolov7.yaml', help='model.yaml path')
+    parser.add_argument('--cfg', type=str, default='cfg/training/yolov7x.yaml', help='model.yaml path')
     parser.add_argument('--data', type=str, default='data/kitti.yaml', help='data.yaml path')
     parser.add_argument('--hyp', type=str, default='data/hyp.scratch.p5.yaml', help='hyperparameters path')
-    parser.add_argument('--epochs', type=int, default=300)
+    parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--batch-size', type=int, default=32, help='total batch size for all GPUs')
     parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='[train, test] image sizes')
     parser.add_argument('--rect', action='store_true', help='rectangular training')
